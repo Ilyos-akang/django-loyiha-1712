adca8a1b27141bad009ab8d1c8b581e60650867f
ilyos <oydinov01@gmail.com>
2025-12-19 20:47:05 +0500

    tugadi

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,9 +5,11 @@ from .views import *
 urlpatterns = [
     path('',index,name='index'),
     path('dashboard/',dashboard,name='dashboard'),
-    path('login/',login,name='login'),
+    path('login/',login_html,name='login'),
+    path('login-enter/',login_page,name='login-enter'),
     path('quiz_page/',quiz_page,name='quiz_page'),
     path('result',result,name='result'),
-    path('settings/',settings,name='settings')
+    path('settings/',settings,name='settings'),
+    path('logout/',login_page,name="logout"),
 ]
 
--- quiz/views.py
+++ quiz/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate ,login,logout
 
 # Create your views here.
 def index(req):
     return render(req,'index.html')
 
 def dashboard(req):
-    return render (req,'dashboard.html')
-
-def login(req):
+    if req.user.is_authenticated:
+        return render (req,'dashboard.html')
+    else:
+        return redirect('login')
+def login_html(req):
     return render(req,'login.html')
+
+def login_page(req):
+    if req.method=='POST':
+        email=req.POST.get("email")
+        parol=req.POST.get("password")
+        print(email,parol)
+        user=authenticate(req,username=email,password=parol)
+        
+        if user is not None:
+            login(req,user)
+            return redirect ("dashboard")        
+    return redirect("login")
+
+def logoutp_page(req):
+    if req.user.is_authenticated:
+        logout(req)
+    return redirect ("login")
 
 def quiz_page(req):
     return render(req,'quiz_page.html')
